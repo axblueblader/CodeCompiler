dd334601d2feaab5701f7425a65fdfd5325881a2
hydrugion <ttkiet16@apcs.vn>
2018-07-16 17:22:57 +0700

    Bladder

--- app/src/main/java/com/kvapps/codecompiler/GetRawFromLink.java
+++ app/src/main/java/com/kvapps/codecompiler/GetRawFromLink.java
@@ -1,11 +1,28 @@
 package com.kvapps.codecompiler;
 
 import android.os.AsyncTask;
+import android.util.Log;
 
-public class GetRawFromLink extends AsyncTask<String, String, String> {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
 
+public class GetRawFromLink extends AsyncTask<URL, Void, String> {
     @Override
-    protected String doInBackground(String... strings) {
-        return null;
+    protected String doInBackground(URL... urls) {
+        String string = "";
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urls[0].openStream()));
+            String content = "";
+            while ((content = bufferedReader.readLine()) != null){
+                string += content;
+                string += '\n';
+            }
+            Log.d("kiet", string);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return string;
     }
 }
